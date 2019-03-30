9227b3502637e3369e3f427e8c25f35ff5dde576
miwa-suekado <bremen-undercover@ezweb.ne.jp>
2019-03-30 10:17:53 +0900

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @books = Book.page(params[:page]).per(4)
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: "書籍を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def show
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+    
+    if @book.update(book_params)
+      redirect_to @book, notice: "書籍を更新しました。"
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    
+    @book.destroy
+    redirect_to books_path, notice: "書籍を削除しました。"
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :price, :publish_date,
+                                :description, :new_image, :category_id)
+  end
+  def set_book
+    @book = Book.find(params[:id])
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+categories = %w(文学・評論 人文・思想 社会・政治･法律 ノンフィクション 歴史・地理 ビジネス 科学・テクノロジー 医学 コンピュータ・IT アート 趣味・実用 スポーツ・アウトドア 資格 暮らし 旅行ガイド コミック ライトノベル)
+
+categories.each do |category|
+  Category.create(name: category)
+end
